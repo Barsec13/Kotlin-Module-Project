5f7b3339a46bda3d05e745e5d34a5d0319deb323
Aleksey Putilov <putilov0407@gmail.com>
2022-12-09 07:01:32 +0500

    Проектная работа №2. Попытка №1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,35 @@
 fun main() {
-    // ваш код начнется здесь
-    // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-    println("Привет")
+
+        val dataBase = Archive()
+        val archives = dataBase.archives
+
+
+        while (true){
+                menu("Архив", archives.keys)
+
+                val inputArchive = input( archives.size + 1).toInt()
+
+                when (inputArchive) {
+                        0 -> addArchive(archives)
+                        archives.size + 1 -> break
+                        else -> while(true){
+
+                                archives[archives.keys.elementAt(inputArchive - 1)]?.
+                                let { menu("Заметки", it.keys) }
+
+                                val inputNote = input(
+                                        archives[archives.keys.elementAt(inputArchive - 1)]?.size!! + 1).toInt()
+
+                                when (inputNote){
+                                        0 -> archives[archives.keys.elementAt(inputArchive - 1)]?.let {addNote(it)}
+                                        (archives[archives.keys.elementAt(inputArchive - 1)]?.size!! + 1) -> break
+                                        else ->archives[archives.keys.elementAt(inputArchive - 1)]?.let {
+                                                openNote(it, inputNote)
+                                        }
+                                }
+                        }
+                }
+        }
+        println("Программа завершила работу")
 }
+
